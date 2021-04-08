597373382a986f0f0e029d90b0212793bb960f22
Andy <ahua049@aucklanduni.ac.nz>
2021-04-09 00:27:12 +1200

    Rebase to main

---
+++ frontend/src/App.jsx
@@ -0,0 +1,57 @@
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import { AppBar, Toolbar, IconButton, Typography } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
+import RoomPage from './pages/RoomPage';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    flexFlow: 'column',
+    height: '100vh',
+    flex: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+  main: {
+    height: '100%',
+  },
+}));
+
+export default function App() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <AppBar position="static">
+        <Toolbar>
+          <IconButton className={classes.menuButton} edge="start" color="inherit">
+            <MenuIcon />
+          </IconButton>
+          <Typography className={classes.title} variant="h6" color="inherit">
+            Dank memes
+          </Typography>
+          <Typography variant="h6" color="inherit">
+            Room Code: 12345
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      <main className={classes.main}>
+        <Switch>
+          <Route path="/room">
+            <RoomPage />
+          </Route>
+
+          <Route path="*">
+            <Redirect to="/room" />
+          </Route>
+        </Switch>
+      </main>
+    </div>
+  );
+}
---
+++ frontend/src/components/Playlist.jsx
@@ -0,0 +1,41 @@
+import {
+  List,
+  ListItem,
+  Divider,
+  ListItemText,
+  ListItemAvatar,
+  Avatar,
+  Typography,
+} from '@material-ui/core';
+
+const dummyPlaylist = [
+  {
+    title: 'title',
+    artist: 'artist',
+    image:
+      'https://www.pinclipart.com/picdir/middle/579-5791475_rocket-emoji-clipart-emoticon-cohete-png-download.png',
+  },
+];
+
+export default function Playlist({ playlist }) {
+  playlist = dummyPlaylist;
+
+  return (
+    <div>
+      <Typography variant="h6">Playlist</Typography>
+      <List>
+        {playlist.map((item, index) => (
+          <>
+            <ListItem alignItems="flex-start">
+              <ListItemAvatar>
+                <Avatar src={item.image} />
+              </ListItemAvatar>
+              <ListItemText primary={item.title} secondary={item.artist} />
+            </ListItem>
+            {index !== playlist.length - 1 && <Divider component="li" />}
+          </>
+        ))}
+      </List>
+    </div>
+  );
+}
