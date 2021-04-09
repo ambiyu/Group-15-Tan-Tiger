6b81fac4030d4e198dab210a37f2e70cee004110
khal745 <49116468+khal745@users.noreply.github.com>
2021-04-09 12:09:13 +1200

    Initial backend structure, draft of room data structures and socketio events

---
+++ backend/data/Room.js
@@ -0,0 +1,17 @@
+// Data structure for storing state of a 'room' - this is where users connect to and can 
+// share links to music/videos, and chat to one another.
+
+class Room {
+    constructor(roomID) {
+        this.roomID = roomID;
+        this.users = [];
+        this.chatMessages = []; // Chat won't be used in initial prototypes, but will store for later.
+        this.musicQueue = []; // Could initially store just URL, but could refactor to include votes too etc.
+
+        this.admin = ''; // Can specify which user is admin, might have admin specific privileges 
+    }
+
+    addUser(user) {
+        this.users.push(user)
+    }
+}
