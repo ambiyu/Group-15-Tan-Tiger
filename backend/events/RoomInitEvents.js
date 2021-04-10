9fe02b4b1dd446b086c9d1b5ff16c231cefad6b4
khal745 <49116468+khal745@users.noreply.github.com>
2021-04-10 18:56:35 +1200

    Working test communication between sockets on client and server

--- backend/events/RoomInitEvents.js
+++ backend/events/RoomInitEvents.js
@@ -5,20 +5,35 @@ function handleCreateRoom(io, socket, roomManager) {
     socket.on('createRoom', (userName) => {
         const user = new User(userName);
         const roomCode = roomManager.createNewRoom(user);
+        // For testing
         console.log("Created new room with user in it")
         console.log(roomManager.roomDict);
-        socket.emit("enterRoom", roomCode);
+
+        // Join this client socket to the a specific room. This prevent broadcasting to everyone.
+        socket.join(String(roomCode));
+
+        io.to(String(roomCode)).emit('enterRoom', roomCode);
     });
 }
 
 function handleJoinRoom(io, socket, roomManager) {
-    socket.on('joinRoom', (roomIdAndUserName) => {
-        const roomID = roomIdAndUserName.roomID;
-        const userName = roomIdAndUserName.userName;
+    socket.on('joinRoom', (roomCodeAndUserName) => {
+        const roomCode = roomCodeAndUserName.roomCode;
+        console.log(roomCode)
+        const userName = roomCodeAndUserName.userName;
         const user = new User(userName);
         console.log('Attempting to join room')
-        roomManager.addUserToRoom(user, roomID);
+        roomManager.addUserToRoom(user, roomCode);
         console.log(roomManager.roomDict);
+        console.log(roomCode)
+
+        socket.join(String(roomCode));
+
+        console.log("try to send msg")
+        const broadcast = "HELLO EVERYONE"
+        const msg = "Here's something for only 1 room"
+        io.emit("testAll", broadcast)
+        io.to(String(roomCode)).emit('someInRoomEvent', msg);
     });
 }
 
