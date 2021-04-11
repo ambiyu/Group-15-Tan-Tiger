28da9a00c3d4b518b2aa523b9ba640466a542c74
Saad-Siddiqui27 <52618232+Saad-Siddiqui27@users.noreply.github.com>
2021-04-11 17:44:25 +1200

    styling changes

--- frontend/src/components/join.jsx
+++ frontend/src/components/join.jsx
@@ -79,18 +79,10 @@ export default function Join() {
             >
                 Join Room
         </Button>
-            <Grid container>
-                <Grid item xs>
-                    <Link href="#" variant="body2">
-                        Join Room
-            </Link>
-                </Grid>
-                <Grid item>
-                    <Link href="#" variant="body2">
+        <Link href="#" variant="body2">
                         {"Don't have an account? Sign Up"}
                     </Link>
-                </Grid>
-            </Grid>
+            
         </form>
     
     );
