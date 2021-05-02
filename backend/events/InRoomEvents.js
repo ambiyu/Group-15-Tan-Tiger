53add1da75b3fd949fa07cd99a56c4be5dc0a027
Saad-Siddiqui27 <52618232+Saad-Siddiqui27@users.noreply.github.com>
2021-05-02 22:03:17 +1200

    added queue back-end functionality

--- backend/events/InRoomEvents.js
+++ backend/events/InRoomEvents.js
@@ -5,6 +5,8 @@ function addToQueue(io, socket, roomManager) {
         const room = roomManager.getRoomByCode(roomCode);
         room.addToQueue(url);
 
+        io.to(String(roomCode)).emit('updateQueue', url)
+
     });
 }
 
@@ -12,14 +14,20 @@ function removeFromQueue(io, socket, roomManager) {
     socket.on("removeFromQueue", (url, roomCode) => {
         const room = roomManager.getRoomByCode(roomCode);
         room.removeFromQueue(url);
+
+        io.to(String(roomCode)).emit('remove', url)
     });
 }
 
 function playFirstVideoFromQueue(io, socket, roomManager) {
-    socket.on("playFirst", (userName, roomName, callback) => { });
+    socket.on("playFirst", (userName, roomName, callback) => {
+        //starts playing for all. just using a message paasing for now
+        io.to(String(roomCode)).emit('play', "Start to play");
+    });
 }
 
 module.exports = function (io, socket, roomManager) {
     addToQueue(io, socket, roomManager);
-    removeFromQueue(io, socket, roomManagger);
- };
+    removeFromQueue(io, socket, roomManager);
+    playFirstVideoFromQueue(io, socket, roomManager);
+};
