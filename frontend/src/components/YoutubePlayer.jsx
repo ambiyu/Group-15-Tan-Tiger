229397275db7c7b19365db2910adec423447ae00
megascatterbomb <55725012+megascatterbomb@users.noreply.github.com>
2021-05-08 21:09:31 +1200

    Queue sync (#18)
    * Fixed wrong variable name for youtube video ID
    
    * Basic queue functionality working
    
    * Queue autoplay with timer functionality
    
    * Removed temp testing button from RoomPage
    
    * Pausing and resuming synced
    
    * WIP synchronization with seeking
    
    * Fixed useRoomState not ignoring self-originated pauses
    
    * Pausing and resuming synced
    
    * WIP synchronization with seeking
    
    * Fixed useRoomState not ignoring self-originated pauses
    
    * Fixing duplciation from prior merge
    
    * Fixed resumeVideo not applying callback to timer
    
    Co-authored-by: Andy <ahua049@aucklanduni.ac.nz>

--- frontend/src/components/YoutubePlayer.jsx
+++ frontend/src/components/YoutubePlayer.jsx
@@ -1,9 +1,11 @@
 import YouTube from '@u-wave/react-youtube';
-import { useEffect, useContext } from 'react';
+import { useContext, useEffect, useRef } from 'react';
 import { RoomContext } from '../context/RoomContextProvider';
+import socket from '../Socket';
 
 function YoutubePlayer() {
   const { state, dispatch } = useContext(RoomContext);
+  let playerComponent = useRef(null);
   // console.log(state);
 
   // useEffect(() => {
@@ -13,14 +15,35 @@ function YoutubePlayer() {
   //   // eslint-disable-next-line react-hooks/exhaustive-deps
   // }, [state.currentlyPlaying.timestamp]);
 
+  function pauseVideo(player) {
+    console.log('upstream pause for room ' + state.roomCode + " user " + state.username);
+    socket.emit('pauseVideo', player.getCurrentTime(), state.roomCode, state.username);
+  }
+  function resumeVideo(player) {
+    console.log('upstream resume for room ' + state.roomCode + " user " + state.username);
+    socket.emit('resumeVideo', player.getCurrentTime(), state.roomCode, state.username);
+  }
+
+  useEffect(() => {
+    console.log("should seek to " + state.seekTo);
+    if(state.seekTo !== -1 && playerComponent.current !== null) {
+      let player = playerComponent.current;
+      player.playerInstance.seekTo(state.seekTo);
+      state.seekTo = -1;
+    }
+  }, [state.seekTo]);
+
   return (
     <div className="YoutubePlayer">
       <YouTube
+        ref={playerComponent}
         video={state.currentlyPlaying.videoID}
         startSeconds={state.currentlyPlaying.timestamp}
         autoplay
         disableKeyboard={true}
-        onPause={(e) => console.log(e.target)}
+        paused={state.paused}
+        onPause={(e) => pauseVideo(e.target)}
+        onPlaying={(e) => resumeVideo(e.target)}
       />
     </div>
   );
