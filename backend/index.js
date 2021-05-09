7ad91a5e7f3e214dddc8fcc5ffdc5611b47aed8f
Andy <ahua049@aucklanduni.ac.nz>
2021-05-10 00:21:36 +1200

    Added cors to backend

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,8 @@
+const cors = require("cors");
 const express = require("express");
 const app = express();
+app.use(cors());
+
 const http = require("http");
 const server = http.createServer(app);
 const port = process.env.PORT || 4000;
